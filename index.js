678a9bd3f3c6ab64078ae78c9d3f0376dcfa13b0
kwameasare04 <73545744+kwameasare04@users.noreply.github.com>
2021-04-09 12:31:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    title: "Phone case",
+    price: 23,
+    quantity: 2,
+    category: "Accessories"
+  },
+  {
+    title: "Android phone",
+    price: 150,
+    quantity: 1,
+    category: "Phones"
+  },
+  {
+    title: "Headphones",
+    price: 78,
+    quantity: 1,
+    category: "Accessories"
+  },
+  {
+    title: "Sport Watch",
+    price: 55,
+    quantity: 2,
+    category: "Watches"
+  }
+];
+
+const quantityByCategories = products => products.reduce((ammounts, products) => {
+   ammounts[products.category] = (ammounts[products.category] || 0) + products.quantity;
+    return ammounts;
+}
+,{});
+
+/* Create a function "quantityByCategories" with one parameter "products".
+
+This function "quantityByCategories" should return an object with keys equal to categories and values equal to sum of all quantities in each category.
+*/
+
+console.log(quantityByCategories(products));
+/* {
+  Accessories: 3,
+  Phones: 1,
+  Watches: 2
+} */
